0eba3099e0f1ba5055b04970c8c3a805e0c492bb
Christopher Canel <christopher.canel@icloud.com>
2015-08-19 15:26:12 -0700

    Fix misc. errors in setup scripts.

--- ganglia/setup.sh
+++ ganglia/setup.sh
@@ -12,6 +12,7 @@ done
 # gmeta needs rrds to be owned by nobody
 chown -R nobody /var/lib/ganglia/rrds
 # cluster-wide aggregates only show up with this. TODO: Fix this cleanly ?
+rm -rf /var/lib/ganglia/conf/default.json
 ln -s /usr/share/ganglia/conf/default.json /var/lib/ganglia/conf/
 
 /etc/init.d/gmetad restart
--- mapreduce/init.sh
+++ mapreduce/init.sh
@@ -5,10 +5,11 @@ case "$HADOOP_MAJOR_VERSION" in
   1)
     echo "Nothing to initialize for MapReduce in Hadoop 1"
     ;;
-  2) 
-    wget http://s3.amazonaws.com/spark-related-packages/mr1-2.0.0-mr1-cdh4.2.0.tar.gz 
+  2)
+    wget http://s3.amazonaws.com/spark-related-packages/mr1-2.0.0-mr1-cdh4.2.0.tar.gz
     tar -xvzf mr1-*.tar.gz > /tmp/spark-ec2_mapreduce.log
     rm mr1-*.tar.gz
+    rm -rf mapreduce/hadoop-2.0.0-mr1-cdh4.2.0
     mv hadoop-2.0.0-mr1-cdh4.2.0/ mapreduce/
     ;;
 
